f6b902726247ada8a0f8139c364a7cdf317a47db
Jonas Kneifl <jonas.kneifl@itm.uni-stuttgart.de>
2023-11-02 10:14:25 +0100

    code with wider x scale

--- content/post/crom/code/diffusion.py
+++ content/post/crom/code/diffusion.py
@@ -46,7 +46,7 @@ def solve_diffusion(Nt, Nx, dx, dt, alpha, u_init):
 
 # Parameters
 r = 16  # latent dimensio
-L = 1.0  # Length of the domain
+L = 50.0  # Length of the domain
 T = 0.00025  # Total simulation time
 Nx = 500  # Number of spatial grid points
 Nt = 150  # Number of time steps
@@ -71,53 +71,53 @@ u = np.zeros((Nsim, Nt, Nx + 1))
 # sample different diffusion parameters using lhs
 from scipy.stats.qmc import LatinHypercube
 sampler = LatinHypercube(d=3, seed=0)
-samples = sampler.random(n=Nsim)
+samples = 2000*sampler.random(n=Nsim)
 for i in range(Nsim):
     alpha1, alpha2, alpha3 = samples[i]
     alpha = get_alpha(alpha1, alpha2, alpha3)
     u[i] = solve_diffusion(Nt, Nx, dx, dt, alpha, u_init)
 
 # # Plot the results in subplot 1 x 2
-i_sim = 12
-# fig, ax = plt.subplots(1, 2, figsize=(12, 4), sharey=True)
-# ax[0].plot(x[0], u[i_sim, 0], label='t=0')
-# plt.ylabel('u')
-# plt.xlabel('x')
-# ax[1].plot(x[0], u[i_sim, -1], label='t=T/2')
-# plt.xlabel('x')
-# plt.legend()
-# plt.show()
-# print('a')
+i_sim = 15
+fig, ax = plt.subplots(1, 2, figsize=(12, 4), sharey=True)
+ax[0].plot(x, u[i_sim, 0], label='t=0')
+plt.ylabel('u')
+plt.xlabel('x')
+ax[1].plot(x, u[i_sim, -1], label='t=T/2')
+plt.xlabel('x')
+plt.legend()
+plt.show()
+print('a')
 
 # create gif of diffusion process
-import imageio
-output_dir = ""
-images = []
-for i in range(Nt):
-    # figure without gui
-    plt.ioff()
-    fig = plt.figure(figsize=(8, 8*0.64))
-    plt.plot(x, u[i_sim, i], 'm', label='t=T/2')
-    plt.axvspan(x[0], x[167], facecolor='grey', alpha=samples[i_sim, 0])
-    plt.axvspan(x[1*167], x[2*167], facecolor='grey', alpha=samples[i_sim, 1])
-    plt.axvspan(x[2*167], x[-1], facecolor='grey', alpha=samples[i_sim, 2])
-    plt.xlabel('$x$')
-    plt.ylabel('$u$')
-    plt.ylim([-1, 1])
-    plt.xlim([0, x.max()])
-    plt.title(f'$t={t[i]*1000:.3f}\,$ms')
-    # increase font size
-    plt.rcParams.update({'font.size': 16})
-    # increase label and tick size
-    plt.tick_params(axis='both', which='major', labelsize=16)
-    plt.tight_layout()
-    fig.savefig('gifs/tmp.png')
-    images.append(imageio.imread('gifs/tmp.png'))
-# # repeated gif
-imageio.mimsave('gifs/diffusion.gif', images, loop=1)
+# import imageio
+# output_dir = ""
+# images = []
+# for i in range(Nt):
+#     # figure without gui
+#     plt.ioff()
+#     fig = plt.figure(figsize=(8, 8*0.64))
+#     plt.plot(x, u[i_sim, i], 'm', label='t=T/2')
+#     plt.axvspan(x[0], x[167], facecolor='grey', alpha=samples[i_sim, 0])
+#     plt.axvspan(x[1*167], x[2*167], facecolor='grey', alpha=samples[i_sim, 1])
+#     plt.axvspan(x[2*167], x[-1], facecolor='grey', alpha=samples[i_sim, 2])
+#     plt.xlabel('$x$')
+#     plt.ylabel('$u$')
+#     plt.ylim([-1, 1])
+#     plt.xlim([0, x.max()])
+#     plt.title(f'$t={t[i]*1000:.3f}\,$ms')
+#     # increase font size
+#     plt.rcParams.update({'font.size': 16})
+#     # increase label and tick size
+#     plt.tick_params(axis='both', which='major', labelsize=16)
+#     plt.tight_layout()
+#     fig.savefig('gifs/tmp.png')
+#     images.append(imageio.imread('gifs/tmp.png'))
+# # # repeated gif
+# imageio.mimsave('gifs/diffusion.gif', images, loop=1)
 
 
-x_factor = 50
+x_factor = 1
 # %% train autoencoder on diffusion data
 Ntrain = 20
 u_train = u[:Ntrain].reshape(-1, Nx+1)
@@ -126,92 +126,12 @@ u_test = u[Ntrain:].reshape(-1, Nx+1)
 x_test = np.repeat(x[np.newaxis], (Nsim - Ntrain)*Nt, axis=0)
 samples_test = samples[Ntrain:].reshape(-1, 3)
 
-# %% TensorFlow Implementation
-import tensorflow as tf
-from tensorflow.keras.layers import Input, Dense, Lambda, concatenate
-# tensorflow debug mode
-# tf.config.run_functions_eagerly(True)  # uncomment this line for debugging
-
-# create autoencoder
-class TFAutoencoder(tf.keras.Model):
-    def __init__(self, r, x_dim):
-        super(TFAutoencoder, self).__init__()
-        self.r = r
-        self.x_dim = x_dim
-        # sequential encoder
-        vector_field_input = Input(shape=(Nx+1,))
-        spatial_input = Input(shape=(Nx+1,))
-        z_ = Dense(128, activation='elu')(vector_field_input)
-        z_ = Dense(64, activation='elu')(z_)
-        z = Dense(self.r, activation='elu')(z_)
-
-        # repeat z for every spatial point
-        # z2 = tf.repeat(z[:, tf.newaxis], repeats=Nx+1, axis=1)
-        # x = spatial_input[:, :, tf.newaxis]
-        # decoder_input = concatenate([x, z2], axis=2)
-        # decoder_input = tf.reshape(decoder_input, (-1, self.r + self.x_dim))
-
-        decoder_input = Input(shape=(self.r + self.x_dim))
-        # sequential decoder
-        u_ = Dense(128, activation='elu')(decoder_input)
-        u_ = Dense(128, activation='elu')(u_)
-        u_ = Dense(128, activation='elu')(u_)
-        u = Dense(1)(u_)
-
-        # reshape u
-        # u = tf.reshape(u, (-1, Nx+1))
-        self.encoder = tf.keras.Model(inputs=[vector_field_input], outputs=z)
-        self.decoder = tf.keras.Model(inputs=[decoder_input], outputs=u)
-
-    def call(self, inputs):
-        """
-        :param x: (batchsize x n) positional coordinates
-        :param u: (batchsize x n) vector field values
-        """
-        x, u = inputs
-        # encoder discretized input to latent space
-        z = self.encoder(u)
-
-        # repeat z for every spatial point
-        z2 = tf.repeat(z[:, tf.newaxis], repeats=Nx+1, axis=1)
-        x = x[:, :, tf.newaxis]
-        decoder_input = concatenate([x, z2], axis=2)
-        decoder_input = tf.reshape(decoder_input, (-1, self.r + self.x_dim))
-
-        # decoder input
-        u_rec = self.decoder(decoder_input)
-
-        u_rec = tf.reshape(u_rec, (-1, Nx+1))
-
-        return u_rec
-
-    def train_step(self, data):
-        x, u = data[0]
-        with tf.GradientTape() as tape:
-            u_rec = self([x, u])
-            loss = tf.reduce_mean(tf.square(u_rec - u))
-        grads = tape.gradient(loss, self.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
-        return {"loss": loss}
-
-# create model
-model = TFAutoencoder(r=16, x_dim=1)
-model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), loss='mse')
-# train_hist = model.fit(x=[x_factor*x_train, u_train], y=u_train, epochs=600, batch_size=24)
-
-# u_rec = model([x_factor*x_train, u_train]).numpy()
-# plt.plot(u_rec[298])
-# plt.plot(u_train[298])
 
 # %% PyTorch Implementation
 import torch
 from torch import nn
 from torch.utils.data import DataLoader, TensorDataset
-from torch.autograd import Variable
-from torch import optim
-from torch.nn import functional as F
 from torch.utils.data import DataLoader
-from torch.utils.data.dataset import random_split
 # create dataset
 dataset = TensorDataset(torch.from_numpy(x_factor*x_train).float(), torch.from_numpy(u_train).float())
 train_loader = DataLoader(dataset, batch_size=16, shuffle=True)
@@ -281,7 +201,7 @@ class TorchAutoencoder(nn.Module):
 # create model
 model = TorchAutoencoder(r=r, Nx=u_train.shape[1], Nd=1)
 criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.0001, weight_decay=1e-5)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=1e-5)
 
 # train model
 if True:
@@ -311,52 +231,52 @@ model.load_state_dict(torch.load(f'model_{x_factor}.pth'))
 
 
 # %% test autoencoder
-print('a')
+
 # reconstruct data
 i_sample = 148
 u_rec = model(torch.from_numpy(x_factor*x_train).float(), torch.from_numpy(u_train).float()).detach().numpy()
 plt.plot(u_rec[i_sample])
 plt.plot(u_train[i_sample])
-
+plt.show()
 
 u_rec_test = model(torch.from_numpy(x_factor*x_test).float(), torch.from_numpy(u_test).float()).detach().numpy()
 # create gif of reconstruction
-import imageio
-output_dir = ""
-images = []
-i_sim = 1
-for i in range(i_sim*Nt, (i_sim+1)*Nt):
-    # figure without gui
-    plt.ioff()
-    # plot with gui
-
-    fig = plt.figure(figsize=(8, 8*0.64))
-    plt.plot(x_test[0], u_test[i], 'm', label='FOM')
-    plt.plot(x_test[0], u_rec_test[i], 'c', label='Reconstruction')
-    plt.axvspan(x_test[0, 0], x_test[0, 167], facecolor='grey', alpha=samples_test[i_sim, 0])
-    plt.axvspan(x_test[0, 1*167], x_test[0, 2*167], facecolor='grey', alpha=samples_test[i_sim, 1])
-    plt.axvspan(x_test[0, 2*167], x_test[0, -1], facecolor='grey', alpha=samples_test[i_sim, 2])
-    plt.xlabel('$x$')
-    plt.ylabel('$u$')
-    plt.ylim([-1, 1])
-    plt.xlim([0, x_test.max()])
-    # legend with fixed position upper right
-    plt.legend(loc='upper right', bbox_to_anchor=(1.0, 1.0))
-    plt.title(f'$t={t[i%Nt]*1000:.3f}\,$ms')
-    # increase font size
-    plt.rcParams.update({'font.size': 16})
-    # increase label and tick size
-    plt.tick_params(axis='both', which='major', labelsize=16)
-    plt.tight_layout()
-    fig.savefig('gifs/tmp.png')
-    images.append(imageio.imread('gifs/tmp.png'))
-# repeated gif
-imageio.mimsave(f'gifs/diffusion_rec_test_{i_sim+1}.gif', images, loop=1)
+# import imageio
+# output_dir = ""
+# images = []
+# i_sim = 1
+# for i in range(i_sim*Nt, (i_sim+1)*Nt):
+#     # figure without gui
+#     plt.ioff()
+#     # plot with gui
+#
+#     fig = plt.figure(figsize=(8, 8*0.64))
+#     plt.plot(x_test[0], u_test[i], 'm', label='FOM')
+#     plt.plot(x_test[0], u_rec_test[i], 'c', label='Reconstruction')
+#     plt.axvspan(x_test[0, 0], x_test[0, 167], facecolor='grey', alpha=samples_test[i_sim, 0])
+#     plt.axvspan(x_test[0, 1*167], x_test[0, 2*167], facecolor='grey', alpha=samples_test[i_sim, 1])
+#     plt.axvspan(x_test[0, 2*167], x_test[0, -1], facecolor='grey', alpha=samples_test[i_sim, 2])
+#     plt.xlabel('$x$')
+#     plt.ylabel('$u$')
+#     plt.ylim([-1, 1])
+#     plt.xlim([0, x_test.max()])
+#     # legend with fixed position upper right
+#     plt.legend(loc='upper right', bbox_to_anchor=(1.0, 1.0))
+#     plt.title(f'$t={t[i%Nt]*1000:.3f}\,$ms')
+#     # increase font size
+#     plt.rcParams.update({'font.size': 16})
+#     # increase label and tick size
+#     plt.tick_params(axis='both', which='major', labelsize=16)
+#     plt.tight_layout()
+#     fig.savefig('gifs/tmp.png')
+#     images.append(imageio.imread('gifs/tmp.png'))
+# # repeated gif
+# imageio.mimsave(f'gifs/diffusion_rec_test_{i_sim+1}.gif', images, loop=1)
 
 # %% latent dynamics
 i_test = 0
-support_point_indices = [51, 205, 315, 261, 214, 144, 356, 267, 115, 271, 244, 309,  62, 364,
-        412, 467, 249, 403, 175, 328, 105, 354]
+support_point_indices = [0, 51, 205, 315, 261, 214, 144, 356, 267, 115, 271, 244, 309,  62, 364,
+        412, 467, 249, 403, 175, 328, 105, 354, 500]
 Nsupport = len(support_point_indices)
 
 x_support = torch.from_numpy(x[support_point_indices]*x_factor).float().requires_grad_()
@@ -374,7 +294,7 @@ def forward(x, z):
     # repeat z for every point in x
     z = z.unsqueeze(0).repeat(n, 1)
     # add new axis to x in the middle
-    x_support = x.unsqueeze(1)
+    x = x.unsqueeze(1)
     # concatenate x and z
     decoder_input = torch.cat((x, z), dim=1)
     # cast to float
@@ -398,11 +318,17 @@ for t in range(1, Nt):
     u_old = forward(x_support, z[t-1])
     # compute the second order spatial gradient using autograd
     hessian = torch.func.hessian(forward, argnums=0)(x_support, z[t-1])
-    u_xx = torch.diagonal(torch.diagonal(hess_api, dim1=0, dim2=1), dim1=0, dim2=1).detach()
+    u_xx = torch.diagonal(torch.diagonal(hessian, dim1=0, dim2=1), dim1=0, dim2=1).detach()
+    # apply boundary conditions
+    for i in range(len(support_point_indices)):
+        if index[i] == 0 or index[i] == n_points - 1:
+            u_xx[i] = 0
+
     # get time derivative
     u_t = alpha_support * u_xx
     # update solution using a first-order Euler scheme
-    u_new = u_old + dt * u_t
+    # u_new = u_old + dt * u_t
+    u_new = u_old + u_t
 
     # evolve latent variable in time (this part uses the linerized version instead of Gauss-Newton solver)
     res = u_t
@@ -417,11 +343,17 @@ for t in range(1, Nt):
 z2 = z.numpy()
 # reconstruct full vector field
 
-u_rec_start = forward(torch.from_numpy(x).float(), z[0]).detach().numpy()
-u_rec_end = forward(torch.from_numpy(x).float(), z[-1]).detach().numpy()
+u_rec_start = forward(torch.from_numpy(x_factor*x).float(), z[0]).detach().numpy()
+u_rec_end = forward(torch.from_numpy(x_factor*x).float(), z[-1]).detach().numpy()
 
 plt.plot(u_rec_start)
-plt.plot(u_rec_end)
+plt.plot(u_rec_end, "--")
+plt.legend(['start', 'end'])
+plt.show()
+
+
+plt.plot(z[:, 0])
+plt.show()
 
 u_init
 u_xx2 = (u_init[2:] - 2 * u_init[1:-1] + u_init[:-2]) / (dx ** 2)
